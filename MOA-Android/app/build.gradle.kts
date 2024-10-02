85c6ec25bb721927f1083a7fa54f2636aff27050
Yoonji Lee <yooonji08@naver.com>
2024-10-02 23:06:40 +0900

    📦️ Build: 타겟 sdk 버전 업그레이드

--- MOA-Android/app/build.gradle.kts
+++ MOA-Android/app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
     defaultConfig {
         applicationId = "com.swuperpoint.moa_android"
         minSdk = 28
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
