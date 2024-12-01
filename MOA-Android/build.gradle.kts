12dcfc0cfa1e9a44a43474979d2a2f00498543c5
Yoonji Lee <yooonji08@naver.com>
2024-12-02 04:50:39 +0900

    📦️ Build: Gson, 카카오맵, Retrofit 라이브러리 추가 #4

--- MOA-Android/build.gradle.kts
+++ MOA-Android/build.gradle.kts
@@ -12,4 +12,5 @@ plugins {
     alias(libs.plugins.kapt) apply false
     alias(libs.plugins.androidx.navigation.safeargs) apply false
     alias(libs.plugins.compose.compiler) apply false
+    alias(libs.plugins.kotlin.parcelize) apply false
 }
